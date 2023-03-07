5b574e233d6109a00bb8e0300341ecbd49fdb383
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-03-07 15:00:22 -0800

    dynamic sign out now displays profile picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ import {
   Stack,
   useColorModeValue,
   useColorMode,
+  Circle
 } from '@chakra-ui/react';
 import { Link as ReactLink } from 'react-router-dom';
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
@@ -42,8 +43,9 @@ const NavLink = ({ path, children }) => (
 const Navbar = () => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
+  const { user, isAuthenticated } = useAuth0();
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} style={{zIndex: 10, position: 'sticky'}} >
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} style={{ zIndex: 10, position: 'sticky' }} >
       <Flex h={16} alignItems='center' justifyContent='space-between'>
         <IconButton
           size='md'
@@ -65,69 +67,76 @@ const Navbar = () => {
                 {link.linkName}
               </NavLink>
             ))}
+          </HStack>
         </HStack>
-      </HStack>
-      <Flex alignItems='center'>
-        <NavLink>
-          <Icon
-            as={colorMode === 'light' ? MoonIcon : SunIcon}
-            alignSelf='center'
-            onClick={() => toggleColorMode()}
-          ></Icon>
-        </NavLink>
-        {useAuth0().isAuthenticated ?
-          <Button
-            as={ReactLink}
-            to='/logout'
-            p={2}
-            fontSize='sm'
-            fontWeight={400}
-            variant='link'>
-            Sign Out
-          </Button>
-          :
-          <Button
-            as={ReactLink}
-            to='/login'
-            p={2}
-            fontSize='sm'
-            fontWeight={400}
-            variant='link'
-          >
-            Sign In
-          </Button>}
-
-        <Button
-          as={ReactLink}
-          to='/registration'
-          m={2}
-          display={{ base: 'none', md: 'inline-flex' }}
-          fontSize='sm'
-          fontWeight={600}
-          _hover={{ bg: 'orange.400' }}
-          bg='blue.500'
-          color='white'
-        >
-          Sign Up
-        </Button>
-      </Flex>
-    </Flex>
-    {isOpen ? (
-      <Box pb={4} display={{ md: 'none' }}>
-        <Stack as='nav' spacing={4}>
-          {links.map((link) => (
-            <NavLink key={link.linkName} path={link.path}>
-              {link.linkName}
-            </NavLink>
-          ))}
-          <NavLink key='sign up' path='/registration'>
-            Sign Up
+        <Flex alignItems='center'>
+          <NavLink>
+            <Icon
+              as={colorMode === 'light' ? MoonIcon : SunIcon}
+              alignSelf='center'
+              onClick={() => toggleColorMode()}
+            ></Icon>
           </NavLink>
-        </Stack>
-      </Box>
-    ) : null}
-  </Box>
-);
+          {isAuthenticated ?
+            <>            <Button
+              as={ReactLink}
+              to='/logout'
+              p={2}
+              fontSize='sm'
+              fontWeight={400}
+              variant='link'>
+              Sign Out
+            </Button>
+              <Circle size='40px'>
+                <img
+                  src={user.picture}
+                  style={{ borderRadius: '20px' }}></img>
+              </Circle>
+            </>
+
+            :
+            <Button
+              as={ReactLink}
+              to='/login'
+              p={2}
+              fontSize='sm'
+              fontWeight={400}
+              variant='link'
+            >
+              Sign In
+            </Button>}
+
+          <Button
+            as={ReactLink}
+            to='/registration'
+            m={2}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize='sm'
+            fontWeight={600}
+            _hover={{ bg: 'orange.400' }}
+            bg='blue.500'
+            color='white'
+          >
+            Sign Up
+          </Button>
+        </Flex>
+      </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as='nav' spacing={4}>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+            <NavLink key='sign up' path='/registration'>
+              Sign Up
+            </NavLink>
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
 };
 
 export default Navbar;
